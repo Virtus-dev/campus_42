0667317523c910886c745d2c04ff75537e26aa55
virtus-dev <aritzgonzalez92@gmail.com>
2022-10-05 09:24:02 +0200

    itoa terminado

--- libft/ft_itoa.c
+++ libft/ft_itoa.c
@@ -13,16 +13,49 @@
 #include "libft.h"
 
 //Entero minimo - 2 147 483 648
-
-char	*ft_itoa(int n)
+static int	ft_digit_count(long int i)
 {
-	int	maxInt;
+	int	count;
 
-	minInt = 2147483648;
-	while (n < maxInt)
+	count = 0;
+	if (i < 0)
 	{
-		if (n < 9)
-			return (n + '0');
-		else
-		{
-			
+		i *= -1;
+		count++;
+	}
+	while (i > 0)
+	{
+		i /= 10;
+		count++;
+	}
+	return (count);
+}
+
+char		*ft_itoa(int n)
+{
+	char		*str;
+	int			i;
+	long int	nb;
+
+	nb = n;
+	i = ft_digit_count(nb);
+	if (!(str = malloc(i * sizeof(char) + 1)))
+		return (0);
+	str[i--] = 0;
+	if (nb == 0)
+	{
+		str = ft_calloc(2, sizeof(char));
+		str[0] = 48;
+	}
+	if (nb < 0)
+	{
+		str[0] = '-';
+		nb = nb * -1;
+	}
+	while (nb > 0)
+	{
+		str[i--] = nb % 10 + '0';
+		nb = nb / 10;
+	}
+	return (str);
+}
