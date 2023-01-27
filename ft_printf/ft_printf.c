2c65e722332ddc014dd9c7bfb72162850548000d
virtus-dev <aritzgonzalez92@gmail.com>
2023-01-27 11:56:46 +0100

    close to finish printf

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,12 +6,48 @@
 /*   By: arigonza < arigonza@student.42malaga.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/14 13:11:36 by arigonza          #+#    #+#             */
-/*   Updated: 2023/01/15 18:41:29 by arigonza         ###   ########.fr       */
+/*   Updated: 2023/01/25 17:55:56 by arigonza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_printf.h";
+#include "ft_printf.h"
 
-int	ft_printf(const char *param, ...)
+void	ft_format(va_list args, const char format, int *count)
+{
+	if (format == 'c')
+		ft_putchar(va_arg(args, int), count);
+	if (format == 's')
+		ft_putstr(va_arg(args, char *), count);
+	if (format == 'd'|| format == 'i')
+		ft_putnbr(va_arg(args,int), count);
+	if (format == 'u')
+		ft_putunsignednbr(va_arg(args, unsigned int), count);
+	if (format == 'x' || format == 'X')
+		ft_printhexa(va_arg(args, int), format, count);
+	if (format == '%')
+		ft_putchar('%', count);
+}
+
+int	ft_printf(const char *string, ...)
 {	
+	va_list	args;
+	int	i;
+	int	count;
+
+	i = 0;
+	count = 0;
+	va_start(args, string);
+	while (string[i])
+	{
+		if (string[i] == '%')
+		{
+			
+			ft_format(args, string[++i], &count);
+			i += 2;
+		}
+		else
+			ft_putchar(string[i++], &count);
+	}
+	va_end(args);
+	return (count);
 }
--- ft_printf/ft_printf_utils.c
+++ ft_printf/ft_printf_utils.c
@@ -6,33 +6,77 @@
 /*   By: arigonza < arigonza@student.42malaga.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/15 18:30:06 by arigonza          #+#    #+#             */
-/*   Updated: 2023/01/15 18:54:37 by arigonza         ###   ########.fr       */
+/*   Updated: 2023/01/27 11:49:37 by arigonza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-void	ft_putchar(char *c)
+void	ft_putchar(int c, int *count)
 {
 	write(1, &c, 1);
+	(*count)++;
 }
 
-void	ft_putnbr(int n)
+void	ft_putnbr(int n, int *count)
 {
 	if (n == -2147483648)
-		ft_putnbr("-2147483648");
+		ft_putstr("-2147483648", count);
 	else
 	{
 		if (n < 0)
 		{
-			ft_putchar("-");
+			ft_putchar('-', count);
 			n = n * -1;
 		}
 
 		if (n > 9)
-		{
-			ft_putnbr(n / 10);
-			ft_putnbr(n % 10 + '0');
-		}
+			ft_putnbr(n / 10, count);
+		ft_putchar((n % 10) + '0', count);
 	}
+}
+
+void	ft_putstr(char *str, int *count)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		ft_putchar(str[i++], count);
+}
+
+void	ft_putunsignednbr(unsigned int nbr, int *count)
+{
+	if (nbr > 9)	
+		ft_putunsignednbr(nbr / 10, count);
+	ft_putchar((nbr % 10) + '0', count);
+}
+
+void	ft_printhexa(unsigned long nbr, char h,int *count)
+{
+	int	i;
+	char	*base;
+	char	str[25];
+
+	i = 0;
+	if (h == 'x')
+		base = "0123456789abcdef";
+	if (h == 'X')
+		base = "0123456789ABCDEF";
+	if (nbr == 0)
+		return (ft_putchar('0', count));
+	while (nbr > 0)
+	{
+		str[i] = base[nbr % 16];
+		nbr = nbr / 16;
+		i++;
+	}
+	while (i--)
+		ft_putchar(str[i], count);
+}
+
+void	printptr(void *ptr, int *count)
+{
+	ft_putstr("0X", count);
+	ft_printhexa((unsigned long)ptr, 'x', count);
 }
